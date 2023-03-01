56708679b93f2fa0e4a58eae12fc7444a22c3a66
Varsha <varsha.inno@aesl.in>
2023-03-01 15:35:44 +0530

    Added my project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DirectChat from './DirectChat';
+
+//Using arrow function component
+const App = () => {
+  return (
+    <DirectChat/>
+  )
+}
+
+export default App
+
+
